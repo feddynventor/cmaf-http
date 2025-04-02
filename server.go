66eaf1b4befaf422c82ab328abce0fbb50426ffa
Feddy's Thinkpad <cavaliere12.web@gmail.com>
2025-04-02 19:12:59 +0200

    segment indexing

--- server.go
+++ server.go
@@ -17,20 +17,32 @@ type Server struct {
 }
 
 type Manifest struct {
-	Config   Ingester            `json:"config"`
-	Start    time.Time           `json:"start"`
-	Epoch    uint64              `json:"epoch"`
-	Last     *Fragment           `json:"last"`
-	Forecast map[string][]uint32 `json:"representations"`
+	Config          Ingester                   `json:"config"`
+	Start           time.Time                  `json:"start"`
+	Epoch           uint64                     `json:"epoch"`
+	Representations map[string]*Representation `json:"representations"`
+	Keyframes       map[string][]*Fragment     `json:"keyframes"`
 }
 
 func (stream *InputStream) Serve() {
 	http.HandleFunc("/"+stream.repr.Id+"/", func(w http.ResponseWriter, r *http.Request) {
-		index, err := strconv.ParseUint(r.URL.Path[strings.LastIndex(r.URL.Path, "/")+1:], 10, 32)
+		index, noIndexProvided := strconv.ParseUint(r.URL.Path[strings.LastIndex(r.URL.Path, "/")+1:], 10, 64)
+
 		w.Header().Set("Content-Type", "application/octet-stream")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Timing-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Expose-Headers", "ruddr-head-pts, ruddr-segment-length")
 
-		if err != nil {
+		// useful for client sync with server
+		if f := stream.GetLastFragment(); f != nil {
+			w.Header().Set("Ruddr-Head-Pts", fmt.Sprintf("%f", f.Pts))
+		} else {
+			w.WriteHeader(http.StatusNotAcceptable)
+			return // stream not yet initialized
+		}
+
+		if noIndexProvided != nil {
+			w.Header().Set("Cache-Control", "no-cache")
 			w.WriteHeader(http.StatusOK)
 			// io.Copy(w, bytes.NewReader(stream.moov))  // TODO: compare
 			if _, err := w.Write(stream.moov); err != nil {
@@ -39,22 +51,39 @@ func (stream *InputStream) Serve() {
 			return
 		}
 
-		frag, ok := stream.fragments.Load(uint32(index))
-		if !ok {
+		// Failsafe - return a segment starting from the keyframe which references the requested fragment
+		fragment, _ := stream.GetPlayableFragment(uint32(index))
+		if fragment == nil {
+			// IMPR: you can redirect 302 to the correct resource or segment
 			w.WriteHeader(http.StatusNotFound)
 			fmt.Fprintf(w, "Fragment %d not found", index)
 			return
 		}
 
-		serveFile(w, r, frag.(*Fragment).fd, int64(frag.(*Fragment).ByteLength))
+		segment, amount := stream.GetNextFragments(fragment)
+		if segment == nil {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, "Segment starting from fragment %d not complete yet", index)
+			return
+		}
+
+		w.Header().Set("Ruddr-Segment-Length", fmt.Sprintf("%d", amount)) // length in fragments
+		// the next keyframed fragment can be calculated as = current + amount
+
+		fds := make([]*memfd.Memfd, 0)
+		segmentSize := int64(0)
+		for _, frag := range segment {
+			fds = append(fds, frag.fd)
+			segmentSize += int64(frag.ByteLength)
+		}
+
+		serveFile(w, r, fds, segmentSize)
 	})
 
 }
 
-func serveFile(w http.ResponseWriter, r *http.Request, fd *memfd.Memfd, size int64) {
+func serveFile(w http.ResponseWriter, r *http.Request, fds []*memfd.Memfd, size int64) {
 	w.Header().Set("Content-Length", fmt.Sprintf("%d", size))
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Timing-Allow-Origin", "*")
 	w.WriteHeader(http.StatusOK)
 
 	// Ensure headers are flushed before hijacking
@@ -83,15 +112,17 @@ func serveFile(w http.ResponseWriter, r *http.Request, fd *memfd.Memfd, size int
 	defer tcpFile.Close()
 	tcpFd := int(tcpFile.Fd())
 
-	offset := int64(0)
-	for offset < size {
-		n, err := unix.Sendfile(tcpFd, int(fd.Fd()), &offset, int(size-offset))
-		if err != nil {
-			fmt.Println("sendfile failed:", err)
-			return
-		}
-		if n == 0 {
-			break
+	for _, fd := range fds {
+		offset := int64(0)
+		for offset < size {
+			n, err := unix.Sendfile(tcpFd, int(fd.Fd()), &offset, int(size-offset))
+			if err != nil {
+				fmt.Println("sendfile failed:", err)
+				return
+			}
+			if n == 0 {
+				break
+			}
 		}
 	}
 }
